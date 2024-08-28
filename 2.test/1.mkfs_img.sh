01e4f2150130379b50d21d89e6b6260f74081915
Feynman G <gxnorz@gmail.com>
2024-08-28 23:03:53 +0800

    1.mkfs_img.sh: add comparision for bcj
    output as follows:
    |erofs_image                   |orgin_size  |bcj_arm64   |Opt_ratio|
    |--                            |--          |--          |--       |
    |so_zlz4hc_9_C_4096            |13185024    |12754944    |3.26%    |
    |so_zlz4hc_9_C_8192            |12394496    |11915264    |3.87%    |
    |so_zlz4hc_9_C_32768           |11182080    |10682368    |4.47%    |
    |so_zlz4hc_9_C_65536           |10792960    |10264576    |4.90%    |
    |so_zlz4hc_9_C_1048576         |10444800    |9957376     |4.67%    |
    |so_zlzma_C_4096               |8654848     |8400896     |2.93%    |
    |so_zlzma_C_8192               |7921664     |7647232     |3.46%    |
    |so_zlzma_C_32768              |7077888     |6766592     |4.40%    |
    |so_zlzma_C_65536              |6848512     |6524928     |4.72%    |
    |so_zlzma_C_1048576            |6418432     |6094848     |5.04%    |

--- 2.test/1.mkfs_img.sh
+++ 2.test/1.mkfs_img.sh
@@ -5,41 +5,89 @@ MKFS=`which mkfs.erofs`
 DPFS=`which dump.erofs`
 SRC_DIR=${1:-./0.src}
 IMGDIR=./0.image/
+LOG_FILE=${IMGDIR}/mkfs.log
 
 mkdir -p ${IMGDIR}
+(printf '=%.0s' $(seq 1 40) && echo "") | tee -a ${LOG_FILE}
+
 create_erofsimg()
 {
-	local img=$1
-	echo "MKFS [mkfs.erofs $@]"
-	$MKFS $@ ${SRC_DIR}
+	local img_dir=$1
+	local img_name=""
+	shift
+	mkdir -p $img_dir
+	img_name=$img_dir/$(basename $SRC_DIR)$(echo $@ | tr -s ' '| sed  's/\([^a-zA-Z0-9]\)/\_/g' | tr -s '_').img
+	[[ ! -e $img_name ]] && echo "MKFS [mkfs.erofs $img_name $@ ${SRC_DIR}]" | tee -a ${LOG_FILE}
+	[[ ! -e $img_name ]] && $MKFS ${img_name} $@ ${SRC_DIR} >> ${LOG_FILE} 2>&1 && return 0
+	echo "$img_name exists!" | tee -a ${LOG_FILE}
+}
+
+compare_filesize()
+{
+	local org_dir=$1
+	local cmp_dir=$2
+	local name_diff=$3
+	local org_fname
+	local cmp_fname
+	local cur_name
+	local format_str="|%-30s|%-12s|%-12s|%-9s|"
+	local header_str="|%-30s|%-12s|%-12s|%-9s|"
+
+	printf "$header_str\n" "erofs_image" "orgin_size" "${name_diff}" "Opt_ratio"
+	printf "$header_str\n" "--" "--" "--" "--"
+	for f in $(ls $org_dir/* -S)
+	do
+		org_fname=$f
+		cmp_fname=$(basename $f)
+		cur_name=${cmp_fname%%.img}
+		cmp_fname=${cur_name}_${name_diff}.img
+		cmp_fname=$(find $cmp_dir -name $cmp_fname)
+		local org_size=$(stat --printf="%s" $org_fname) 
+		local cmp_size=$(stat --printf="%s" $cmp_fname)
+		local opt_ratio=$(echo $org_size $cmp_size | awk '{print (1-$2/$1)*100}' | xargs printf "%.2f%%")
+		printf "$format_str\n" $cur_name $org_size $cmp_size "${opt_ratio}"
+	done
 }
 
 #test mkfs.erofs
 mkimg_raw()
 {
-	create_erofsimg  ${IMGDIR}/erofs-raw32.img -E force-inode-compact || exit -11
-	create_erofsimg  ${IMGDIR}/erofs-raw64.img -E force-inode-extended || exit -12
+	#erofs-raw32.img
+	create_erofsimg  ${IMGDIR}/ -E force-inode-compact || exit ${LINENO}
+	#erofs-raw64.img
+	create_erofsimg  ${IMGDIR}/ -E force-inode-extended || exit ${LINENO}
 }
 
 mkimg_lzma()
 {
-	create_erofsimg  ${IMGDIR}/erofs-lzma-4k.img -zlzma,9 || exit -13
-	create_erofsimg  ${IMGDIR}/erofs-lzma-bigpcluster-8k.img -zlzma,9 -C 8192 || exit -14
-	create_erofsimg  ${IMGDIR}/erofs-lzma-rand-bigpcluster-1M.img -zlzma,9 -C 1048576 --random-pclusterblks || exit -15
+	#erofs-lzma-4k
+	create_erofsimg  ${IMGDIR}/ -zlzma,9 -C 4096 || exit ${LINENO}
+	create_erofsimg  ${IMGDIR}/ -zlzma,9 -C 8192 || exit ${LINENO}
+	#create_erofsimg  ${IMGDIR}/ -zlzma,9 -C 1048576 --random-pclusterblks || exit ${LINENO}
 }
 
-mkimg_lz4()
+erofs_mkimg()
 {
-	create_erofsimg  ${IMGDIR}/erofs-lz4-4k.img -zlz4hc,9 || exit -16
-	create_erofsimg  ${IMGDIR}/erofs-lz4-bigpcluster-1M.img -zlz4hc,9 -C 1048576 | exit -17
-	#create_erofsimg  ${IMGDIR}/erofs-lz4-rand-bigpcluster-1M.img -zlz4hc,9 -C 1048576 --random-pclusterblks || exit -17
+	local cluster
+	local cluster_list="4 8 32 64 1024"
+
+	for cluster in $cluster_list
+	do
+		create_erofsimg  ${IMGDIR}/origin -zlz4hc,9 -C $((cluster * 1024))  || exit ${LINENO}
+		create_erofsimg  ${IMGDIR}/       -zlz4hc,9 -C $((cluster * 1024)) --bcj-arm64 || exit ${LINENO}
+		create_erofsimg  ${IMGDIR}/origin -zlzma    -C $((cluster * 1024))  || exit ${LINENO}
+		create_erofsimg  ${IMGDIR}/       -zlzma    -C $((cluster * 1024)) --bcj-arm64 || exit ${LINENO}
+	done
 }
 
 mkimg_random()
 {
-	create_erofsimg  ${IMGDIR}/erofs-rand-alg-1M.img -zlz4:lzma,9:lz4hc,9 -C 1048576 --random-pclusterblks --random-algorithms || exit -18
-	create_erofsimg  ${IMGDIR}/erofs-rand-frag-1M.img -zlz4:lzma,9:lz4hc,9 -C 8192 -Efragments,8192 --random-pclusterblks --random-algorithms || exit -19
-	create_erofsimg  ${IMGDIR}/erofs-rand-frag-dedupe-1M.img -zlz4:lzma,9:lz4hc,9 -C 1048576 -Efragments,1048576 -Ededupe --random-algorithms || exit -20
+	#erofs-rand-alg-1M.img
+	create_erofsimg  ${IMGDIR}/-zlz4:lzma,9:lz4hc,9 -C 1048576 --random-pclusterblks --random-algorithms || exit ${LINENO}
+	#erofs-rand-frag-1M.img
+	create_erofsimg  ${IMGDIR}/ -zlz4:lzma,9:lz4hc,9 -C 8192 -Efragments,8192 --random-pclusterblks --random-algorithms || exit ${LINENO}
+	#erofs-rand-frag-dedupe-1M.img
+	create_erofsimg  ${IMGDIR}/ -zlz4:lzma,9:lz4hc,9 -C 1048576 -Efragments,1048576 -Ededupe --random-algorithms || exit ${LINENO}
 }
 
 #test {dump,fsck}.erofs
@@ -54,4 +102,5 @@ test_dump_fsck_erofs()
 	ls -F ${IMGDIR}/
 }
 
-mkimg_lz4
+erofs_mkimg
+compare_filesize ${IMGDIR}/origin ${IMGDIR}/ bcj_arm64
